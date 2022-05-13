a5fc773286a95d7af0e8ea66bc295137887ae7dd
onomeh <onwuanibeonome@gmail.com>
2022-05-13 10:29:42 +0000

    Solution

---
+++ 05_13_2022.js
@@ -0,0 +1,19 @@
+function getGrade (s1, s2, s3) {
+  // Code here
+  let sum = s1 + s2 + s3
+  let average = sum/3
+  if(average >= 90){
+return 'A'
+  }
+  else if(average >=80 ){
+return 'B'
+}
+  else if(average >= 70) {
+return 'C'
+    }
+  else if(average >= 60 ){
+return 'D'
+    }else{
+      return 'F'
+    }
+  }
